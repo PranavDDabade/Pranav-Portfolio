9b94342050e287067fc06f148d22dd429c1b1799
Pranav Dabade <pranavddabade@gmail.com>
2025-07-13 14:39:48 +0530

    Update Hero.jsx

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../assets/profile_img.png'
-import resume from '../assets/resume.pdf'
+import React from 'react';
+import './Hero.css';
+import profile_img from '../assets/profile_img.png';
+import resume from '../assets/resume.pdf';
+
 const hero = () => {
     return (
         <div className='hero'>
             <img src={profile_img} alt="pranav" width={300} />
             <h1><span>I am Pranav Dabde,</span> DevOps Engineer based in India</h1>
-            <p>Aspiring and growth-focused DevOps Engineer currently building a strong foundation in cloud platforms, CI/CD pipelines, Infrastructure as Code (IaC), and automation. Actively learning how to bridge the gap between development and operations to enable faster, more reliable software delivery. Passionate about exploring modern DevOps tools, cloud technologies, and best practices to drive continuous improvement and innovation.</p>
+            <p>
+                Aspiring and growth-focused DevOps Engineer currently building a strong foundation in cloud platforms, CI/CD pipelines, Infrastructure as Code (IaC), and automation. Actively learning how to bridge the gap between development and operations to enable faster, more reliable software delivery. Passionate about exploring modern DevOps tools, cloud technologies, and best practices to drive continuous improvement and innovation.
+            </p>
             <div className="hero-action">
-                <div className="hero-connect" ><a href="#contact">Connect with me</a></div>
-                <div className="hero-resume"><a href={resume} download="PranavResume.pdf" alt="Resume">Resume</a></div>
+                <a href="#contact" className='hero-connect' >Connect with me</a>
+                <a href={resume} download="PranavResume.pdf" className="hero-resume">Resume</a>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default hero
+export default hero;
