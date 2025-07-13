14611dd2dbe0253f6f93d0f7bcf6a903c45e4610
Pranav Dabade <pranavddabade@gmail.com>
2025-07-13 13:55:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './component/Navbar'
-import Hero from './component/hero'
+import Hero from './component/Hero'
 import Aboutme from './component/Aboutme'
 import Education from './component/Education'
 import Skill from './component/Skill'
