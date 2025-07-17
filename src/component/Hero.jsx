d5b699f9dad1289cce0041a22fa6ac1a7f930e19
Pranav Dabade <pranavddabade@gmail.com>
2025-07-17 19:58:17 +0530

    Update Hero.jsx

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -7,7 +7,7 @@ const hero = () => {
     return (
         <div className='hero'>
             <img src={profile_img} alt="pranav" width={300} />
-            <h1><span>I am Pranav Dabde,</span> DevOps Engineer based in India</h1>
+            <h1><span>I am Pranav Dabade,</span> DevOps Engineer based in India</h1>
             <p>
                 Aspiring and growth-focused DevOps Engineer currently building a strong foundation in cloud platforms, CI/CD pipelines, Infrastructure as Code (IaC), and automation. Actively learning how to bridge the gap between development and operations to enable faster, more reliable software delivery. Passionate about exploring modern DevOps tools, cloud technologies, and best practices to drive continuous improvement and innovation.
             </p>
